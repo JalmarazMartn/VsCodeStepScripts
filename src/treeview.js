09302eb72cc5f999b598b2c6b23d19dfdbd5978e
Jesus Almaraz martín <JalmarazMartn@gmail.com>
2021-11-14 08:18:41 +0100

    Treeview form Json Object

--- src/treeview.js
+++ src/treeview.js
@@ -1,7 +1,7 @@
 const vscode = require('vscode');
 module.exports = {
     createTreeView:
-        function (jsonData) { 
+        function (jsonData) {
             //return createTreeView(jsonData); 
             createTreeView(jsonData);
         }
@@ -9,9 +9,7 @@ module.exports = {
 //create vscode tree view from JSON data
 function createTreeView(jsonData) {
     vscode.window.registerTreeDataProvider('treeView', new TreeDataProvider(jsonData));
-    //var treeView = 
     vscode.window.createTreeView('treeView', { treeDataProvider: new TreeDataProvider(jsonData) });
-    //return treeView;
 }
 //create tree view data provider from JSON data
 class TreeDataProvider {
@@ -19,22 +17,62 @@ class TreeDataProvider {
         this._onDidChangeTreeData = new vscode.EventEmitter();
         this.onDidChangeTreeData = this._onDidChangeTreeData.event;
         this._jsonData = jsonData;
-        console.log(this._jsonData);
     }
     getTreeItem(element) {
-        return element;
+        //return element;
+        const value = JSON.parse(element.label);
+        if (typeof value === 'object') {
+            console.log(value);
+            return new vscode.TreeItem(GetKeyLiteralStringFromJson(value), vscode.TreeItemCollapsibleState.Collapsed);
+        }
+        else {
+            return new vscode.TreeItem(JSON.stringify(value), vscode.TreeItemCollapsibleState.None);
+        }
+
+        //return new vscode.TreeItem('Label' + JSON.stringify(this._jsonData), vscode.TreeItemCollapsibleState.None);        
     }
     getChildren(element) {
         if (element) {
             //return element.children;
-            return [new vscode.TreeItem(JSON.stringify(this._jsonData), vscode.TreeItemCollapsibleState.None)];
+            //return [new vscode.TreeItem(JSON.stringify(this._jsonData), vscode.TreeItemCollapsibleState.None)];
+            const value = JSON.parse(element.label);
+            return getTreeItemArrayFromJSON(value);
         }
         else {
-            const JSONItem = new vscode.TreeItem(JSON.stringify(this._jsonData), vscode.TreeItemCollapsibleState.Collapsed);
-            return [JSONItem];            
+            return getTreeItemArrayFromJSON(this._jsonData);
         }
+
     }
     refresh() {
         this._onDidChangeTreeData.fire();
     }
+}
+
+function getTreeItemArrayFromJSON(JSONData) {
+    let itemArray = [];
+    for (var key in JSONData) {
+        const value = JSONData[key];
+        if (typeof value === 'object') {
+            itemArray.push(new vscode.TreeItem(JSON.stringify(value), vscode.TreeItemCollapsibleState.Collapsed));
+        }
+        else {
+            if (JSONData.hasOwnProperty(key)) {
+                itemArray.push(new vscode.TreeItem(JSON.stringify(value), vscode.TreeItemCollapsibleState.None));
+            }
+        }
+    }
+    return itemArray;
+}
+function GetKeyLiteralStringFromJson(jsonData) {
+    for (var key in jsonData) {
+        const value = jsonData[key];
+        if (typeof value === 'object') {
+            return GetKeyLiteralStringFromJson(value);
+        }
+        else {
+            if (jsonData.hasOwnProperty(key)) {
+                return key;
+            }
+        }
+}
 }
