a7583a2c765e04eed87a78f3abf85f9c926ebb3f
Jesus Almaraz martín <JalmarazMartn@gmail.com>
2021-12-03 07:09:20 +0100

    tree view from JSOn completed

--- src/treeview.js
+++ src/treeview.js
@@ -1,4 +1,7 @@
 const vscode = require('vscode');
+let labelNumber = 0;
+let labelArray = [];
+
 module.exports = {
     createTreeView:
         function (jsonData) {
@@ -12,101 +15,94 @@ function createTreeView(jsonData) {
     vscode.window.createTreeView('treeView', { treeDataProvider: new TreeDataProvider(jsonData) });
 }
 //create tree view data provider from JSON data
-class TreeDataProvider {
+class TreeDataProvider {    
     constructor(jsonData) {
+        labelNumber = 0;
+        labelArray = [];
         this._onDidChangeTreeData = new vscode.EventEmitter();
         this.onDidChangeTreeData = this._onDidChangeTreeData.event;
         this._jsonData = jsonData;
     }
     getTreeItem(element) {
-        //return element;
-        const value = JSON.parse(element.label);
-
+        //return element;        
+        const value = getJSONObjectFromlabelID(element.label);
+        let labelName = getLabelNameFromLabelId(element.label);
         if (typeof value === 'object') {
-            return new vscode.TreeItem(GetKeyLiteralStringFromJson(value), vscode.TreeItemCollapsibleState.Collapsed);
+            return new vscode.TreeItem(labelName, vscode.TreeItemCollapsibleState.Collapsed);    
         }
         else {
-            return new vscode.TreeItem(JSON.stringify(value), vscode.TreeItemCollapsibleState.None);
+            labelName = '"' + labelName + '": ';
+            return new vscode.TreeItem(labelName + JSON.stringify(value), vscode.TreeItemCollapsibleState.None);
         }
     }
     getChildren(element) {
-        if (element) {
-            //return element.children;
-            //return [new vscode.TreeItem(JSON.stringify(this._jsonData), vscode.TreeItemCollapsibleState.None)];
-            const value = JSON.parse(element.label);
-            return getTreeItemArrayFromJSON(value, false);
+        if (element) {            
+            const value = getJSONObjectFromlabelID(element.label);
+            return getTreeItemArrayFromJSON(value);
         }
         else {
-            return getTreeItemArrayFromJSON(this._jsonData, true);
+            return getTreeItemArrayFromJSON(this._jsonData);
         }
-
     }
     refresh() {
         this._onDidChangeTreeData.fire();
-    }
+    }    
 }
 
-function getTreeItemArrayFromJSON(JSONData, AddKey = false) {
+function getTreeItemArrayFromJSON(JSONData) {
     let itemArray = [];
-    let names = [];
-    if (JSONData) {
-        names = Object.getOwnPropertyNames(JSONData);
+    let labelName = '';
+    const names = Object.getOwnPropertyNames(JSONData);
+    if (names[0]) {
+        labelName = names[0];
     }
-    let i = -1;
+    let nameNumber = 0;
     for (var key in JSONData) {
-        i = i + 1;
-        const value = JSONData[key];
-        //const label = key + ': ' + JSON.stringify(value);
-        let label = JSON.stringify(value);
-        if (names[i]) {
-            //console.log(names[i]);
-            const objName = '{"' + names[i] + '": ';
-            if (getObjectNameFromJSONObject(JSONData) !== names[i] || AddKey) {
-                label = objName + label + '}';
-            }
-        }
-        console.log('..');
-        console.log('content==>' + i + ' ' + label);
-        if (typeof value === 'object') {
-            itemArray.push(new vscode.TreeItem(label, vscode.TreeItemCollapsibleState.Collapsed));
+        if (names[nameNumber]) {
+            labelName = names[nameNumber];
         }
         else {
-            //if (JSONData.hasOwnProperty(key)) {
-            itemArray.push(new vscode.TreeItem(label, vscode.TreeItemCollapsibleState.None));
-            //}
+            labelName = key;
+        }
+        nameNumber = nameNumber + 1;
+        const value = JSONData[key];        
+        console.log('.');
+        console.log(value);
+        const labelID = pushJSONObjectToArray(value,labelName);    
+        if (typeof value === 'object') {                        
+            itemArray.push(new vscode.TreeItem(labelID, vscode.TreeItemCollapsibleState.Collapsed));
+        }
+        else {
+            itemArray.push(new vscode.TreeItem(labelID, vscode.TreeItemCollapsibleState.None));
         }
     }
     return itemArray;
 }
 
-function GetKeyLiteralStringFromJson(jsonData) {
-    const names = Object.getOwnPropertyNames(jsonData);
-    for (var key in jsonData) {
-        const value = jsonData[key];
-        return key;
-        console.log('.');
-        console.log('content==>' + key + ' ' + JSON.stringify(value));
-        if (names[0]) {
-            console.log('name==>' + names[0]);
-            return names[0];
-        }
-        if (typeof value === 'object') {
-            //return GetKeyLiteralStringFromJson(value);
-            //if (names[0]) {
-            //    console.log('name==>' + names[0]);
-            //    return names[0];
-            //}
-        }
-        //if (jsonData.hasOwnProperty(key)) {
-        //    return key;
-        //}
-        //return key;
-    }
+function pushJSONObjectToArray(jsonData,labelName='') {
+    labelNumber = labelNumber + 1;
+    const labelID = 'label' + labelNumber;    
+    const finalLabel = {
+        "labelID": labelID,
+        "labelData": JSON.stringify(jsonData),        
+        "labelName": labelName
+    };
+    labelArray.push(finalLabel);
+    return labelID;
 }
-function getObjectNameFromJSONObject(jsonData) {
-    const names = Object.getOwnPropertyNames(jsonData);
-    if (names[0]) {
-        return names[0];
+function getJSONObjectFromlabelID(labelID) {
+    for (var i = 0; i < labelArray.length; i++) {
+        if (labelArray[i].labelID === labelID) {
+            return JSON.parse(labelArray[i].labelData);            
+        }
     }
-    return '';
+    return null;
+}
+function getLabelNameFromLabelId(labelID) {
+    for (var i = 0; i < labelArray.length; i++) {
+        if (labelArray[i].labelID === labelID) {
+            return labelArray[i].labelName;            
+        }
+    }
+    return '';    
 }
